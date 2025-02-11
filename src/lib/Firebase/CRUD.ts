b00e8ec762fda5d6877734fef1e5026839ceb283
Dev3 <dev3@wegen.com.br>
2025-02-11 08:23:50 -0300

    fix firebase

--- src/lib/Firebase/CRUD.ts
+++ src/lib/Firebase/CRUD.ts
@@ -1,39 +1,33 @@
-
 import { collection, addDoc, doc, updateDoc, arrayUnion } from 'firebase/firestore';
 import { db } from './firebaseConfig';
 
-
 type EmailModel = {
-  confirmed: string[]
-  email: string
-  presents: string[]
-}
+  confirmed: string[];
+  email: string;
+  presents: string[];
+};
 
 // Adicione um documento
 export async function AddProduct(data: EmailModel) {
   try {
-
+    // Adiciona um novo documento à coleção 'guests'
     const docRef = await addDoc(collection(db, 'guests'), data);
-    console.log('Documento ref:', docRef.id);
+    console.log('Documento criado com ID:', docRef.id);
+
     try {
-      const userDocRef = doc(db, 'guests', );
-      console.log('Documento user ID: ', userDocRef.id);
-      // Atualiza o documento adicionando o novo produto ao array 'products'
+      // Atualiza o mesmo documento recém-criado
+      const userDocRef = doc(db, 'guests', docRef.id);
       await updateDoc(userDocRef, {
         products: arrayUnion(docRef.id),
         lastUpdate: new Date()
       });
 
-      console.log(`Produto ${docRef.id} adicionado ao usuário ${data} com sucesso.`);
+      console.log(`Produto ${docRef.id} adicionado com sucesso.`);
     } catch (error) {
-      console.error('Erro ao adicionar produto ao usuário: ', error);
-      throw error; // Lance o erro para ser tratado pelo chamador da função
+      console.error('Erro ao atualizar o documento:', error);
+      throw error;
     }
   } catch (error) {
-    console.error('Erro ao adicionar documento: ', error);
+    console.error('Erro ao adicionar documento:', error);
   }
-
 }
-
-
-
